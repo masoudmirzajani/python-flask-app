dcff3a5ff16f97ad374d8a627e873b0206a6cf26
masoudmirzajani <masoud.mirzajani@gmail.com>
2022-04-10 04:43:02 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 
-from flask import Flask, render_template
+  
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
